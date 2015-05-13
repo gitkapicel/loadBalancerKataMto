22a21ea80547faa22d374868e837912698ee98aa
Lukasz Tracz <kapicel20wp.pl>
2015-05-13 23:36:50 +0200

    Test 3 green

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -3,8 +3,10 @@ package edu.iis.mto.serverloadbalancer;
 public class Server {
 
 	public double currentLoadPecentage;
+	public int capacity;
 
 	public Server(int capacity) {
+		this.capacity = capacity;
 	}
 
 	
